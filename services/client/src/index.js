423c727d322cec131412f26227fbfe90d6f73674
benhoyle <benjhoyle@gmail.com>
2018-05-09 13:30:12 +0100

    Add User form functionality

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -8,8 +8,12 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            users: []
+            users: [],
+            username: '',
+            email: '',
         };
+        this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     };
     componentDidMount() {
         this.getUsers();
@@ -19,6 +23,24 @@ class App extends Component {
             .then((res) => { this.setState({ users: res.data.data.users }); })
             .catch((err) => { console.log(err); })
     };
+    addUser(event) {
+        event.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+        .then((res) => {
+            this.getUsers();
+            this.setState({ username: '', email: '' });
+        })
+        .catch((err) => { console.log(err); });
+    };
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    };
     render() {
         return (
             <div className="container">
@@ -27,7 +49,12 @@ class App extends Component {
                         <br/>
                         <h1>All Users</h1>
                         <hr/><br/>
-                        <AddUser/>
+                        <AddUser
+                            username={this.state.username}
+                            email={this.state.email}
+                            handleChange={this.handleChange}
+                            addUser={this.addUser}
+                        />
                         <hr/><br/>
                         <UsersList users={this.state.users}/>
                     </div>
