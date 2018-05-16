f309b035e4df1548ebf11ce953ac23cf72e288fe
benhoyle <benjhoyle@gmail.com>
2018-05-16 19:36:19 +0100

    Register and Login forms on front end

--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -23,7 +23,7 @@ test('UsersList renders properly', () => {
     const wrapper = shallow(<UsersList users={users}/>);
     const element = wrapper.find('h4');
     expect(element.length).toBe(2);
-    expect(element.get(0).props.className).toContain('card');
+    expect(element.get(0).props.className).toContain('well');
     expect(element.get(0).props.children).toBe('ben');
 });
 
